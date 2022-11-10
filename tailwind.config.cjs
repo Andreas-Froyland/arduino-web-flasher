865d77cdea3bd5ccae31ac1d683a3ef625a5770d
Andreas Nordstrand <andreas.sundgot.nordstrand@imenco.com>
2022-11-10 21:00:47 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
